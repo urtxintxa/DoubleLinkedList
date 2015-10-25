43b335342c60aa52908ee09a39627d83090f456d
amaia <amitxu96@gmail.com>
2015-10-25 23:32:47 +0100

    add eginda

--- src/org/minakdev/DoubleLinkedList/UnorderedDoubleLinkedList.java
+++ src/org/minakdev/DoubleLinkedList/UnorderedDoubleLinkedList.java
@@ -4,18 +4,59 @@ public class UnorderedDoubleLinkedList<T> extends DoubleLinkedList<T> implements
 	
 	public void addToFront(T elem) {
 	// hasieran gehitu
-		// KODEA OSATU ETA KOSTUA KALKULATU
+		Node<T> aux = new Node<T>(elem);
+		if(this.count==0){
+			this.first = aux;
+			this.last = aux;
+		}
+		else {
+			aux.next = this.first;
+			this.first.prev = aux;
+			this.first = aux;
+		}
+		this.count++;
 
 	}
 
 	public void addToRear(T elem) {
 	// bukaeran gehitu
-		// KODEA OSATU ETA KOSTUA KALKULATU
+		Node<T> aux = new Node<T>(elem);
+		if(this.count==0){
+			this.first = aux;
+			this.last = aux;
+		}
+		else {
+			aux.prev = this.last;
+			this.last.next = aux;
+			this.last = aux;
+		}
+		this.count++;
 
 	}
 	
 	public void addAfter(T elem, T target) {
 		// KODEA OSATU ETA KOSTUA KALKULATU (AUKERAZKOA)
+		Node<T> egungoa = first;
+		boolean aurkitua = false;
+		Node<T> aux = new Node<T>(elem);
+		
+		while((egungoa != null) && !aurkitua){
+			if(egungoa.data.equals(target)){
+				aurkitua = true;
+				if(egungoa== last){
+					egungoa.next = aux;
+					aux.prev = egungoa;
+					this.last = aux;
+				}
+				else{
+					aux.prev = egungoa;
+					aux.next = egungoa.next;
+					egungoa.next.prev = aux;
+					egungoa.next = aux;
+				}
+				this.count++;	
+			}
+		}
 	}
 
 }
