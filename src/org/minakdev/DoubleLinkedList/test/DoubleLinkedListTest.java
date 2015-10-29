ad71c1dc3173b5a2cac86b122e0e81d8d377932b
DOCENCIA <DOCENCIA@10.227.79.128>
2015-10-29 09:31:44 +0100

    JUnit-ak

---
+++ src/org/minakdev/DoubleLinkedList/test/DoubleLinkedListTest.java
@@ -0,0 +1,94 @@
+package org.minakdev.DoubleLinkedList.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class DoubleLinkedListTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testDoubleLinkedList() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetDeskr() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetDeskr() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRemoveFirst() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRemoveLast() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRemove() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFirst() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testLast() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testContains() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFind() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testIsEmpty() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSize() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testIterator() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAdabegiakInprimatu() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testToString() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ src/org/minakdev/DoubleLinkedList/test/UnorderedDoubleLinkedListTest.java
@@ -0,0 +1,34 @@
+package org.minakdev.DoubleLinkedList.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class UnorderedDoubleLinkedListTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testAddToFront() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddToRear() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddAfter() {
+		fail("Not yet implemented");
+	}
+
+}
