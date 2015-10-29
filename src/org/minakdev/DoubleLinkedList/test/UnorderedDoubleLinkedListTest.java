c21d3ac2590c7e0e57ae5d229edbb42065a6992e
DOCENCIA <DOCENCIA@10.227.79.128>
2015-10-29 09:50:51 +0100

    Unordered Test-a

--- src/org/minakdev/DoubleLinkedList/test/UnorderedDoubleLinkedListTest.java
+++ src/org/minakdev/DoubleLinkedList/test/UnorderedDoubleLinkedListTest.java
@@ -5,30 +5,43 @@ import static org.junit.Assert.*;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.minakdev.DoubleLinkedList.UnorderedDoubleLinkedList;
 
 public class UnorderedDoubleLinkedListTest {
+	
+	private UnorderedDoubleLinkedList<Integer> unordered;
 
 	@Before
 	public void setUp() throws Exception {
+		unordered = new UnorderedDoubleLinkedList<Integer>();
 	}
 
 	@After
 	public void tearDown() throws Exception {
+		unordered = null;
 	}
 
 	@Test
 	public void testAddToFront() {
-		fail("Not yet implemented");
+		unordered.addToFront(2);
+		unordered.addToFront(1);
+		assertTrue(unordered.first().equals(1));
+		assertTrue(unordered.last().equals(2));
 	}
 
 	@Test
 	public void testAddToRear() {
-		fail("Not yet implemented");
+		unordered.addToRear(2);
+		unordered.addToRear(1);
+		assertTrue(unordered.first().equals(2));
+		assertTrue(unordered.last().equals(1));
 	}
 
 	@Test
 	public void testAddAfter() {
-		fail("Not yet implemented");
+		unordered.addToFront(1);
+		unordered.addAfter(2, 1);
+		assertTrue(unordered.last().equals(2));
 	}
 
 }
